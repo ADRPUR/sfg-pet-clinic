aa2fecf8414e449f2bde2c9926525c4bd61c20e5
ADRPUR <apurice@gmail.com>
2020-12-08 18:11:23 +0200

    Implement interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/md/purice/sfgpetclinic/services/PetService.java
@@ -0,0 +1,15 @@
+package md.purice.sfgpetclinic.services;
+
+
+import md.purice.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
