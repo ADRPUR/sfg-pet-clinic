e3db0203897e0cc11070ec6f277f7a4a21aa60b2
ADRPUR <apurice@gmail.com>
2020-12-08 21:19:50 +0200

    refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/md/purice/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/md/purice/sfgpetclinic/services/PetService.java
@@ -3,13 +3,6 @@ package md.purice.sfgpetclinic.services;
 
 import md.purice.sfgpetclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
