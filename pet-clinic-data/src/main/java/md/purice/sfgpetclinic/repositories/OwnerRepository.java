869528a1a231818fffcf246a604ce4cd915d1540
ADRPUR <apurice@gmail.com>
2020-12-17 13:31:03 +0200

    impl fin owner. closes #54

--- pet-clinic-data/src/main/java/md/purice/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/md/purice/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,7 +3,11 @@ package md.purice.sfgpetclinic.repositories;
 import md.purice.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/md/purice/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/md/purice/sfgpetclinic/services/OwnerService.java
@@ -2,9 +2,13 @@ package md.purice.sfgpetclinic.services;
 
 import md.purice.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
